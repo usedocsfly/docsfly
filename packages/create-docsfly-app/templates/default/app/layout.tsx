1363a4398b79c529143d6b1115055aa19bb599d5
rai <raic_dev@proton.me>
2025-07-13 02:45:34 +0900

    fix: update package version to 1.3.0 and correct MainLayout import path

--- packages/create-docsfly-app/templates/default/app/layout.tsx
+++ packages/create-docsfly-app/templates/default/app/layout.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { MainLayout } from "docsfly";
+import { MainLayout } from "docsfly/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
