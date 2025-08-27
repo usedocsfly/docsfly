78a889f637977ed83728722c2b54be0162c6cf1a
rai <raic_dev@proton.me>
2025-08-27 16:19:45 +0900

    feat: Bump version to 1.6.0 for create-docsfly-app and docsfly; remove obsolete test file

--- packages/docsfly/test.js
+++
@@ -1,8 +0,0 @@
-// Test the built package
-try {
-  const docsfly = require('./dist/index.js');
-  console.log('Available exports:', Object.keys(docsfly));
-  console.log('MainLayout:', typeof docsfly.MainLayout);
-} catch (error) {
-  console.error('Error loading package:', error.message);
-}
